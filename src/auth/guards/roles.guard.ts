b1f4fcb3211edc765e420f30fb4cef5f9b53d951
Danendra Indiarto <indiartodanendra@gmail.com>
2026-05-17 18:31:05 +0700

    finall

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -12,19 +12,31 @@ export class RolesGuard implements CanActivate {
       context.getClass(),
     ]);
 
-    // PENTING
-    // kalau endpoint tidak pakai @Roles()
-    // maka otomatis boleh akses
+    /**
+     * Jika endpoint tidak memakai @Roles()
+     * maka otomatis diizinkan
+     */
     if (!requiredRoles) {
       return true;
     }
 
-    const { user } = context.switchToHttp().getRequest();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const request = context.switchToHttp().getRequest();
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const user = request.user;
+
+    /**
+     * Jika user belum login
+     */
     if (!user) {
       return false;
     }
 
+    /**
+     * Cek role user
+     */
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
     return requiredRoles.includes(user.role);
   }
 }
