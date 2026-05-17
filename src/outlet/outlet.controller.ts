76ac0eef14705a79e1cc00729e6c0288336cd8e2
Danendra Indiarto <indiartodanendra@gmail.com>
2026-05-17 21:14:36 +0700

    backend expense

--- src/outlet/outlet.controller.ts
+++ src/outlet/outlet.controller.ts
@@ -7,7 +7,6 @@ import {
   Patch,
   Post,
   UploadedFile,
-  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 
@@ -18,55 +17,55 @@ import { OutletService } from './outlet.service';
 import { CreateOutletDto } from './dto/create-outlet.dto';
 import { UpdateOutletDto } from './dto/update-outlet.dto';
 
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { RolesGuard } from 'src/auth/guards/roles.guard';
-
-import { Roles } from 'src/auth/decorators/roles.decorator';
-
 @Controller('outlets')
-@UseGuards(JwtAuthGuard, RolesGuard)
 export class OutletController {
   constructor(private readonly outletService: OutletService) {}
 
-  @Roles('SUPER_ADMIN')
   @Post()
   @UseInterceptors(FileInterceptor('qrisImage'))
   create(
-    @Body() dto: CreateOutletDto,
+    @Body()
+    dto: CreateOutletDto,
 
-    @UploadedFile() file: Express.Multer.File,
+    @UploadedFile()
+    file: Express.Multer.File,
   ) {
     return this.outletService.create(dto, file);
   }
 
-  @Roles('SUPER_ADMIN')
   @Get()
   findAll() {
     return this.outletService.findAll();
   }
 
-  @Roles('SUPER_ADMIN')
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(
+    @Param('id')
+    id: string,
+  ) {
     return this.outletService.findOne(id);
   }
 
-  @Roles('SUPER_ADMIN')
   @Patch(':id')
   @UseInterceptors(FileInterceptor('qrisImage'))
   update(
-    @Param('id') id: string,
+    @Param('id')
+    id: string,
 
-    @Body() dto: UpdateOutletDto,
+    @Body()
+    dto: UpdateOutletDto,
 
-    @UploadedFile() file: Express.Multer.File,
+    @UploadedFile()
+    file: Express.Multer.File,
   ) {
     return this.outletService.update(id, dto, file);
   }
 
-  @Roles('SUPER_ADMIN')
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(
+    @Param('id')
+    id: string,
+  ) {
     return this.outletService.remove(id);
   }
 }
