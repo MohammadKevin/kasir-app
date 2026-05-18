de90141e4633457fa7778adce970a7d4fac449b4
Mohammad Kevin <kvn4.200581@gmail.com>
2026-05-18 12:16:19 +0700

    final

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 
 import { UserService } from './user.service';
 
@@ -10,38 +16,42 @@ import { Roles } from 'src/auth/decorators/roles.decorator';
 @Controller('users')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+  ) {}
 
-  // SUPER ADMIN ONLY
+  // GET KARYAWAN
+  @Roles('SUPER_ADMIN')
+  @Get('karyawan')
+  getKaryawan() {
+    return this.userService.getKaryawan();
+  }
+
+  // GET ALL USERS
   @Roles('SUPER_ADMIN')
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
-  // SUPER ADMIN ONLY
-  @Roles('SUPER_ADMIN')
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(id);
-  }
-
-  // SUPER ADMIN ONLY
-  @Roles('SUPER_ADMIN')
-  @Delete(':id')
-  delete(@Param('id') id: string) {
-    return this.userService.delete(id);
-  }
-
-  // SUPER ADMIN ONLY
+  // GET PROFILE
   @Roles('SUPER_ADMIN', 'ADMIN')
   @Get(':id/profile')
   profile(@Param('id') id: string) {
     return this.userService.profile(id);
   }
 
-  @Get("karyawan")
-  getKaryawan() {
-    return this.userService.getKaryawan();
+  // GET DETAIL USER
+  @Roles('SUPER_ADMIN')
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  // DELETE USER
+  @Roles('SUPER_ADMIN')
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(id);
   }
 }
