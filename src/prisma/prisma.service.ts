8b8c712c0f8a6a60b70e4af3cd77728f9dd61fea
MohammadKevin <kvn4.200581@gmail.com>
2026-06-05 01:04:21 +0700

    remove runtime admin seed

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,50 +1,25 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+} from '@nestjs/common'
+
+import {
+  PrismaClient,
+} from '@prisma/client'
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  private readonly logger = new Logger(PrismaService.name);
-
+export class PrismaService
+  extends PrismaClient
+  implements
+    OnModuleInit,
+    OnModuleDestroy
+{
   async onModuleInit() {
-    await this.$connect();
-
-    await this.seedAdmin();
+    await this.$connect()
   }
 
-  private async seedAdmin() {
-    const adminName = process.env.ADMIN_NAME;
-    const adminEmail = process.env.ADMIN_EMAIL;
-    const adminPassword = process.env.ADMIN_PASSWORD;
-
-    if (!adminName || !adminEmail || !adminPassword) {
-      this.logger.warn(
-        'ADMIN_NAME, ADMIN_EMAIL, ADMIN_PASSWORD belum diset',
-      );
-      return;
-    }
-
-    const existingAdmin = await this.admin.findUnique({
-      where: {
-        email: adminEmail,
-      },
-    });
-
-    if (existingAdmin) {
-      this.logger.log('Admin sudah ada');
-      return;
-    }
-
-    const hashedPassword = await bcrypt.hash(adminPassword, 10);
-
-    await this.admin.create({
-      data: {
-        name: adminName,
-        email: adminEmail,
-        password: hashedPassword,
-      },
-    });
-
-    this.logger.log(`Admin ${adminEmail} berhasil dibuat`);
+  async onModuleDestroy() {
+    await this.$disconnect()
   }
 }
